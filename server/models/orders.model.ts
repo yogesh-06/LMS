ac95e887b1f78c693799b8094850fae99eb16bc1
Yogesh Rajput <86060505+yogesh-06@users.noreply.github.com>
2026-09-22 18:05:59 +0530

    Created schemas for Orders and Notification models

---
+++ server/models/orders.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+  courseId: string;
+  userId: string;
+  paymentInfo: object;
+}
+
+const orderSchema = new Schema<IOrder>(
+  {
+    courseId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    paymentInfo: { type: Object },
+  },
+  { timestamps: true },
+);
+
+const OrderModel: Model<IOrder> = mongoose.model("Order", orderSchema);
+
+export default OrderModel;
